46dc5597b959b9774d649b63b1a37fe840ddb0fb
Daryl Graves <d.graves88@outlook.com>
2024-12-12 12:01:16 +0000

    Day 9 Part 1 Complete.

--- 2024/Day_09/Day09_Part_1.ps1
+++ 2024/Day_09/Day09_Part_1.ps1
@@ -1,102 +1,54 @@
-$Troubleshooting = $false
 $Data = Get-Content .\Input.txt
-$UnpackedText = [System.Collections.Generic.List[char]]::new()
-Clear-Host
 
-# Populate the char[]
+$AllValues = [System.Collections.Generic.List[long]]::new()
+
 $Id = 0
 for ($x = 0; $x -lt $Data.Length; $x += 2) {
-    $Files = ($Id.ToString() * [int][string]$Data[$x]).ToCharArray()
-    $Whitespace = ("." * [int][string]$Data[$x + 1]).ToCharArray()
+    $noOfFiles = [int][string]$Data[$x]
+    $noOfWhiteSpace = [int][string] $Data[$x + 1]
 
-    foreach ($file in $Files) {
-        $UnpackedText.Add($File)
+    for ($i = 0; $i -lt $noOfFiles; $i++) {
+        $AllValues.Add($Id)
     }
 
-    foreach ($space in $whitespace) {
-        $UnpackedText.Add($space)
+    for ($i = 0; $i -lt $noOfWhiteSpace; $i++) {
+        $AllValues.Add(-1)
     }
     $Id += 1
 }
 
-if ($Troubleshooting) {
-    Write-Host $UnpackedText
-}
+# Re-sort
+$Back = $AlLValues.Count - 1
+for ($x = 0; $x -lt $AllValues.Count; $x++) {
+    # Skip if there is already a valid Id
+    if ($AllValues[$x] -ge 0) {
+        continue
+    }
 
-$KeepRunning = $true
-$Front = 0
-$Back = $UnpackedText.Count - 1
+    # Skip if AllValues is empty space
+    while ($AllValues[$Back] -eq -1) {
+        $Back -= 1
+        continue
+    }
 
-do {
-    # Front and back have reached/passed each other
-    if ($Front -ge $Back) {
-        $KeepRunning = $false
+    # Finish if the front has gone past the back
+    if ($x -ge $Back) {
         break
     }
 
-    # Reached the end of the char[]
-    if (($Front -ge $UnpackedText.Count) -or $UnpackedText[$Front] -eq ' ') {
-        $KeepRunning = $false 
-        continue 
-    }
-
-    # File already present, move forward one
-    if ($UnpackedText[$Front] -ne '.') {
-        $Front += 1 
-        continue
-    }
-
-    # Back has a .
-    if ($UnpackedText[$Back] -eq '.') {
-        $Back -= 1
-        continue
-    }
-    
-    if ($UnpackedText[$Back] -ne ' ') { 
-        $toMove = $UnpackedText[$Back]
-        $UnpackedText[$Back] = '.'
-        $UnpackedText[$Front] = $toMove
-
-        if ($Troubleshooting) {
-            [System.Console]::SetCursorPosition(0, 1)
-            Write-Host "                                                                                                                                                                                                                                                                                                             "
-            [System.Console]::SetCursorPosition(0, 0)
-            Write-Host "                                                                                                                                                                                                                                                                                                             "
-            [System.Console]::SetCursorPosition(0, 0)
-            Write-Host $UnpackedText
-            [System.Console]::SetCursorPosition($Front * 2, 1)
-            Write-Host "^" -ForegroundColor Green
-            [System.Console]::SetCursorPosition(($Back * 2) - 2, 1)
-            Write-Host "^" -ForegroundColor Red
-            # Start-Sleep -Seconds  1
-        }
-
-        $Front += 1
-        $Back -= 1
-
-    }
-} while (
-    $KeepRunning
-)
-
-$UnpackedText -join "" | Out-File temp.txt
-
-[long] $Results = 0
-for ($x = 0; $x -lt ($UnpackedText.Count - 1); $x++) {
-    $Value = $UnpackedText[$x]
-
-    try {
-        $ResultToAdd = $X * [int][string]$Value
-        if (($Troubleshooting)) {
-            Write-Host "Adding $X * $([int][string]$Value) = $ResultToAdd"
-        }
-        $Results += $ResultToAdd
-    }
-    catch {
-        if (($Troubleshooting)) {
-            Write-Host "Skipping as not a number"
-        }
-    }
+    $Moving = $AllValues[$Back]
+    $AllValues[$x] = $Moving
+    $AllValues[$Back] = -1
+    $Back -= 1
 }
 
-Write-Host $Results
+
+$Result = 0
+for ($x = 0; $x -lt $AllValues.Count; $x++) {
+    if ($AllValues[$x] -eq -1) {
+        break
+    }
+    $Result += ($AllValues[$x] * $x)
+}
+
+Write-Host $Result
